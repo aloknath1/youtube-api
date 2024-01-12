3e05971492200bbd623126e8496cacb6f8a49c6b
aloknath1 <nath.alok59@gmail.com>
2024-01-12 12:58:06 +0530

    remove the api key

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,4 @@
 import {data} from '../data/playlist';
-const API_KEY = 'AIzaSyBBQGPiW7Gxo8i0LINWSARHwYL-3LS9Djs';
-const channelID = "UCmFt1y9cbHx3amPRctMEyvA";
-
 
 let API_LIST = {
    // SEARCH: `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelID}&part=snippet,id&order=date&maxResult=20`,
